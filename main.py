08a81cb8536dd6a0b93e8c950deefc6c06e2b2d5
Данил Галашов <danil.galashov@gmail.com>
2022-11-24 15:21:32 +0500

    lab03

---
+++ main.py
@@ -0,0 +1,12 @@
+N = 10
+a = []
+for i in range(N):
+    a.append(float(input("Input number: ")))
+print(a)
+
+for i in range(N - 1):
+    for j in range(N - i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(a)
